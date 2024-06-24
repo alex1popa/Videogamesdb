88257119e8dcbfd72b39805187ecf27734012ffb
Tanko Szilard <szilard-a.tanko@student.unitbv.ro>
2024-06-25 00:47:37 +0300

    Made password actually look like a password

--- src/app/auth/auth-component/auth.component.ts
+++ src/app/auth/auth-component/auth.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { LogInPayload } from '../_helpers/interfaces/login.payload';
+import { AuthService } from '../_helpers/services/auth.service';
 
 @Component({
   selector: 'app-auth',
