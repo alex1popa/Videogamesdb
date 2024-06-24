d74d78ce6d13fd2569e3424fe33ea741fe21bf8e
Alex Popa <alexpopa1910@gmil.com>
2024-06-24 17:00:26 +0300

    Am adaugat componentele auth si table

--- src/app/table/table.module.ts
+++ src/app/table/table.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TableRoutingModule } from './table-routing.module';
+import { TableComponent } from './table/table.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TableComponent
+  ],
   imports: [
     CommonModule,
     TableRoutingModule
