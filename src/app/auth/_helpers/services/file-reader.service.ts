5389889ee6d5509dfc59ec21158ba271c83324d7
Tanko Szilard <szilard-a.tanko@student.unitbv.ro>
2024-06-26 21:26:19 +0300

    Now you can register (it doesn't work)

--- src/app/auth/_helpers/services/file-reader.service.ts
+++ src/app/auth/_helpers/services/file-reader.service.ts
@@ -1,16 +1,51 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject} from 'rxjs';
+import { LogInPayload } from '../interfaces/login.payload';
+//import * as fs from 'fs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ConfigService {
+export class FileReaderService {
   private configUrl = 'assets/credentials.json';
+  private configSubject: BehaviorSubject<LogInPayload[]>;
 
-  constructor(private http: HttpClient) { }
-
-  getConfig(): Observable<{ email: string, password: string }> {
-    return this.http.get<{ email: string, password: string }>(this.configUrl);
+  constructor(private http: HttpClient) { 
+    this.configSubject = new BehaviorSubject<LogInPayload[]>([]);
+    this.loadConfig();
   }
+
+  getConfig(): Observable<{ credentials: LogInPayload[] }> {
+    return this.http.get<{  credentials: LogInPayload[] }>(this.configUrl);
+  }
+  
+  private loadConfig(): void {
+    this.http.get<LogInPayload[]>(this.configUrl).subscribe(
+      (config) => {
+        this.configSubject.next(config);
+      },
+      (error) => {
+        console.error('Failed to load config', error);
+      }
+    );
+  }
+  // addCredential(email: string, password: string): void {
+  //   const newCredential: LogInPayload = { email, password };
+  //   const currentConfig = this.configSubject.getValue();
+  //   const updatedCredentials = [...currentConfig, newCredential];
+  //   this.configSubject.next(updatedCredentials);
+
+  //   localStorage.setItem('appConfig', JSON.stringify(updatedCredentials));
+
+  //   const jsonContent = JSON.stringify(updatedCredentials);
+  //   fs.writeFile("./alphabet.json", jsonContent, 'utf8', function (err) {
+  //     if (err) {
+  //         return console.log(err);
+  //     }
+  
+  //     console.log("The file was saved!");
+  // }); 
+  // }
+
 }
--- src/app/auth/auth-component/auth.component.ts
+++ src/app/auth/auth-component/auth.component.ts
@@ -3,39 +3,40 @@ import { Router } from '@angular/router';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { LogInPayload } from '../_helpers/interfaces/login.payload';
 import { AuthService } from '../_helpers/services/auth.service';
-import { ConfigService } from '../_helpers/services/file-reader.service';
+import { FileReaderService } from '../_helpers/services/file-reader.service';
+import { first } from 'rxjs';
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
-  rememberMe: boolean = false;
-  email: string ='';
-  password: string ='';
+  email: string = '';
+  password: string = '';
+  emailRegister: string = '';
+  passwordRegister: string = '';
+  passwordConfirmationRegister: string = '';
+  firstNameRegister: string = '';
+  lastNameRegister: string = '';
+
   userToken: string | null = '';
-
-  loginEmail: string = '';
-  loginPassword: string = '';
-
-
-
+  credentials: LogInPayload[] = [];
   constructor(
     private authService: AuthService,
-    private router: Router, 
+    private router: Router,
     private notificationService: NzNotificationService,
-    private configService: ConfigService
-  ) {}
+    private fileReaderService: FileReaderService
+  ) { }
 
   ngOnInit(): void {
     this.userToken = this.authService.getToken();
     this.loadConfig();
   }
 
-  loadConfig(): void{
-    this.configService.getConfig().subscribe(config => {
-      this.loginEmail = config.email;
-      this.loginPassword = config.password;
+  loadConfig(): void {
+    this.fileReaderService.getConfig().subscribe(config => {
+      this.credentials = config.credentials;
     });
   }
 
@@ -67,37 +68,29 @@ export class AuthComponent implements OnInit {
     });
   }
 
-  onRememberMeRequest(): void {
-    const payload: LogInPayload = {
-      email: 'eve.holt@reqres.in',
-      password: 'cityslicka',
-    };
-    this.authService.successLogIn(payload).subscribe({
-      next: (response: { token: string; }) => {
-        console.log(response);
-        this.authService.setToken(response.token);
-        this.userToken = this.authService.getToken();
-
-        if (this.rememberMe) {
-          localStorage.setItem('userToken', response.token);
-        } else {
-          sessionStorage.setItem('userToken', response.token);
-        }
-
-        this.router.navigateByUrl('/table');
-      },
-      error: (err: any) => {
-        console.error(err);
-      },
-    });
-  }
-
   onRequest(email: string, password: string): void {
-    
-    if (email === this.loginEmail && password === this.loginPassword) {
+    const credential = this.credentials.find(
+      cred => cred.email === email && cred.password === password
+    );
+
+    if (credential) {
       this.onSuccessRequest();
     } else {
       this.onErrorRequest();
+    }
+  }
+
+  onRegister(): void {
+    const passwordVerifier = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,}$'/;
+    const nameVerifier = /'^[a-zA-Z]{2,}$'/;
+    const emailVerifier = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (passwordVerifier.test(this.passwordRegister) && nameVerifier.test(this.firstNameRegister) && nameVerifier.test(this.lastNameRegister) && emailVerifier.test(this.emailRegister) && this.passwordRegister == this.passwordConfirmationRegister) {
+      //this.fileReaderService.addCredential(this.emailRegister, this.passwordRegister);
+      this.emailRegister = ''; 
+      this.passwordRegister = '';
+      this.firstNameRegister = '';
+      this.lastNameRegister = '';
+      this.passwordConfirmationRegister = '';
     }
   }
 }
